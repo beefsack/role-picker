53a1c4450316721b479ace02513cf84409533dc3
Michael Alexander <beefsack@gmail.com>
2018-12-19 12:46:01 +1100

    Basic project

---
+++ src/components/RolePicker.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface Props { }
+
+export const RolePicker = (props: Props) => <div className="container">
+    <h1 className="title is-1">
+        Role Picker
+    </h1>
+    <h2 className="subtitle is-3">
+        Assign roles randomly but fairly
+    </h2>
+</div>;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { RolePicker } from "./components/RolePicker";
+
+ReactDOM.render(
+    <RolePicker />,
+    document.getElementById("content")
+);
